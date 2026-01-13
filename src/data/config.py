827913fd15447a7337a9113a3f4b7e606526a50c
InzamanCareem <blankfindertechnotommy@gmail.com>
2026-01-13 22:50:52 +0530

    Added File Organizer

---
+++ src/data/config.py
@@ -0,0 +1,13 @@
+"""
+Configuration settings for the medimgprep package.
+
+This module defines default constants used across medimgprep, such as default
+directory names and processing modes.
+
+Constants:
+- DEFAULT_INCLUDE_MODE (bool): Whether to include subfolders by default. Default is False.
+- DEFAULT_OUTPUT_DIR_NAME (str): Default name for the output directory. Default is "original".
+"""
+
+DEFAULT_INCLUDE_MODE: bool = False
+DEFAULT_OUTPUT_DIR_NAME: str = "original"
