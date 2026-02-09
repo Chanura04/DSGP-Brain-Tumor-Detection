803b2a11719bfd677679ed8e33f69da0b0ba8088
InzamanCareem <blankfindertechnotommy@gmail.com>
2026-02-09 09:55:04 +0530

    Seperated batch making process

---
+++ src/utils/batching.py
@@ -0,0 +1,20 @@
+from typing import List, Generator
+from pathlib import Path
+
+def create_batch(iterable: List[Path], n: int) -> Generator[List[Path], None, None]:
+    """
+    Makes batches of the total images to reduce cpu overload,
+    memory usage and have control over certain operation.
+    e.g. to increase efficiency, reduce downtime, and improve consistency.
+
+    :param iterable: image list to make batches
+    :param n: number of images in a batch
+    """
+    batch_list: List[Path] = []
+    for item in iterable:
+        batch_list.append(item)
+        if len(batch_list) == n:
+            yield batch_list
+            batch_list = []
+    if batch_list:
+        yield batch_list
